844423915b3f1e88484546c3eb75c6972ffaa37a
Adrian Maciej <adrian.maciej@netguru.com>
2019-12-16 00:02:29 +0100

    Fixes in pipelines

--- boardgamecrawler/pipelines.py
+++ boardgamecrawler/pipelines.py
@@ -15,15 +15,16 @@ class PriceRegexPipeline():
     patt = re.compile('([\d\.,]*)')
 
     def __process_price(self, item, price):
-        p = item[price]
-        if p:
-            val = self.patt.match(p).group(0)
-            val = val.replace(',' ,'.')
-            try:
-                val = float(val)
-                item[price] = val
-            except Exception as ex:
-                logger.error(ex)
+        if price in item.keys():
+            p = item[price]
+            if p:
+                val = self.patt.match(p).group(0)
+                val = val.replace(',' ,'.')
+                try:
+                    val = float(val)
+                    item[price] = val
+                except Exception as ex:
+                    logger.error(ex)
     
     def process_item(self, item, spider):
         self.__process_price(item, 'price')
@@ -31,7 +32,9 @@ class PriceRegexPipeline():
         return item
 
 class AddToCartPipeline():
+    field_name = 'add_to_cart_available'
 
     def process_item(self, item, spider):
-        item['add_to_cart_available'] = bool(item['add_to_cart_available'])
+        if self.field_name in item:
+            item[self.field_name] = bool(item[self.field_name])
         return item
